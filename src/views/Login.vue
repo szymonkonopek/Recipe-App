3f414bee9aa9565907d2d39e1c34ba639f0b374c
AmeliaLis <lis.amelia.maria@gmail.com>
2024-04-13 17:49:17 +0200

    feat(auth): Login and register page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,37 +1,42 @@
 <template>
-<div style="min-height:90vh">
-  <div class='text-center justify-content-center p-5'><h1 class="'text-center">Welcome back!</h1></div>
-  <div class="container-fluid justify-content-center text-center ">
+<div style="min-height: 90vh">
+  <div class='text-center justify-content-center p-4'><h1 class="text-center">Welcome back!</h1></div>
+  <div class="container-fluid justify-content-center text-center pb-4 px-4">
     <div class="card justify-content-center text-center shadow">
       <div class="card-body">
         <div>
-          <h1>Sign in to Recipe App</h1>
-          <div v-if="error" class="alert alert-danger small-alert">{{ errorToString(error) }}</div>
-          <div class="py-2 my-2">
-            <div class="input-group input-group mb-3 py-2">
-              <span class="input-group-text" id="inputGroup-sizing">Email</span>
-              <input type="text" class="form-control" placeholser="Email" v-model="email" aria-label="Sizing example input" aria-describedby="inputGroup-sizing">
-            </div>
-            <div class="input-group input-group mb-3 py-2">
-              <span class="input-group-text" id="inputGroup-sizing">Password</span>
-              <input type="password" class="form-control" placeholser="Password" v-model="password" aria-label="Sizing example input" aria-describedby="inputGroup-sizing">
-            </div>
-            <div>
-              <button
-                class="btn btn-primary btn-lg text-black my-2 rounded-pill"
-                @click="login"
-              >
-                Submit
-              </button>
-            </div>
-            <div class='text-center justify-content-center p-1 text-muted'>or</div>
-            <div>
-              <button
-                class="btn btn-lg btn-outline-primary my-2 rounded-pill"
-                @click="signInWithGoogle"
-              >
-                Sign in with Google   <img class="img-fluid justify-content-center" style="max-width: 20px;" src="../assets/login/google-icon.png">
-              </button>
+          <div class="row">
+            <div class="col-lg-6"><img src="../assets/login/login.png" class="img-fluid"></div>
+            <div class="col-lg-6">
+              <h2>Sign in to Recipe App</h2>
+              <div v-if="error" class="alert alert-danger small-alert">{{ errorToString(error) }}</div>
+              <div class="py-2 my-2">
+                <div class="input-group input-group mb-3 py-2">
+                  <span class="input-group-text" id="inputGroup-sizing">Email</span>
+                  <input type="text" class="form-control" placeholser="Email" v-model="email" aria-label="Sizing example input" aria-describedby="inputGroup-sizing">
+                </div>
+                <div class="input-group input-group mb-3 py-2">
+                  <span class="input-group-text" id="inputGroup-sizing">Password</span>
+                  <input type="password" class="form-control" placeholser="Password" v-model="password" aria-label="Sizing example input" aria-describedby="inputGroup-sizing">
+                </div>
+                <div>
+                  <button
+                    class="btn btn-primary btn-lg text-black my-2 rounded-pill"
+                    @click="login"
+                  >
+                    Submit
+                  </button>
+                </div>
+                <div class='text-center justify-content-center p-1 text-muted'>or</div>
+                <div>
+                  <button
+                    class="btn btn-lg btn-outline-primary my-2 rounded-pill"
+                    @click="signInWithGoogle"
+                  >
+                    Sign in with Google   <img class="img-fluid justify-content-center" style="max-width: 20px;" src="../assets/login/google-icon.png">
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -39,7 +44,6 @@
     </div>
   </div>
 </div>
-
 </template>
 
 <script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,68 +1,77 @@
 <template>
   <div style="min-height: 90vh">
-    <div style="height: 100px"></div>
-    <div
-      class="container-fluid justify-content-center text-center col-sm-12 col-lg-8 px-4 py-2 mx-auto my-4"
-    >
-      <div
-        class="card justify-content-center text-center w-75 mx-auto auth-form"
-      >
+    <div class='text-center justify-content-center p-4'><h1 class="text-center">Create an account</h1></div>
+    <div class="container-fluid justify-content-center text-center pb-4 px-4">
+      <div class="card justify-content-center text-center shadow">
         <div class="card-body">
           <div>
-            <h1>Register</h1>
-            <div v-if="error" class="alert alert-danger small-alert">
-              {{ errorToString(error) }}
-            </div>
+            <div class="row">
+              <div class="col-lg-6"><img src="../assets/login/signup.png" class="img-fluid"></div>
+              <div class="col-lg-6">
 
-            <div class="py-2 my-2">
-              <div class="input-group input-group-lg py-2">
-                <span class="input-group-text" id="inputGroup-sizing-lg"
-                  >Email</span
-                >
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholser="Email"
-                  v-model="email"
-                  aria-label="Sizing example input"
-                  aria-describedby="inputGroup-sizing-lg"
-                />
-              </div>
-              <div class="input-group input-group-lg py-2">
-                <span class="input-group-text" id="inputGroup-sizing-lg"
-                  >Password</span
-                >
-                <input
-                  type="password"
-                  class="form-control"
-                  placeholser="Password"
-                  v-model="password"
-                  aria-label="Sizing example input"
-                  aria-describedby="inputGroup-sizing-lg"
-                />
-              </div>
-              <div
-                v-if="password.length > 0 && password.length < 8"
-                class="text-danger"
-              >
-                Password is too short (min. 8 characters)
-              </div>
-              <div>
-                <button
-                  class="btn btn-lg btn-outline-dark my-2"
-                  @click="register"
-                  :disabled="password.length < 8"
-                >
-                  Submit
-                </button>
-              </div>
-              <div>
-                <button
-                  class="btn btn-lg btn-outline-dark my-2"
-                  @click="signInWithGoogle"
-                >
-                  Sign in with Google
-                </button>
+                <h2>Register to Recipe App</h2>
+                <div v-if="error" class="alert alert-danger small-alert">
+                  {{ errorToString(error) }}
+                </div>
+
+                <div class="py-2 my-2">
+                  <div class="input-group input-group mb-3 py-2">
+                    <span class="input-group-text" id="inputGroup-sizing"
+                      >Email</span
+                    >
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholser="Email"
+                      v-model="email"
+                      aria-label="Sizing example input"
+                      aria-describedby="inputGroup-sizing"
+                    />
+                  </div>
+
+                  <div class="input-group input-group mb-3 py-2">
+                    <span class="input-group-text" id="inputGroup-sizing"
+                      >Password</span
+                    >
+                    <input
+                      type="password"
+                      class="form-control"
+                      placeholser="Password"
+                      v-model="password"
+                      aria-label="Sizing example input"
+                      aria-describedby="inputGroup-sizing"
+                    />
+                  </div>
+
+                  <div
+                    v-if="password.length > 0 && password.length < 8"
+                    class="text-danger"
+                  >
+                    Password is too short (min. 8 characters)
+                  </div>
+
+                  <div>
+                    <button
+                      class="btn btn-lg btn-primary text-black my-2 rounded-pill"
+                      @click="register"
+                      :disabled="password.length < 8"
+                    >
+                      Submit
+                    </button>
+                  </div>
+
+                  <div class='text-center justify-content-center p-1 text-muted'>or</div>
+                  
+                  <div>
+                    <button
+                      class="btn btn-lg btn-outline-primary my-2 rounded-pill"
+                      @click="signInWithGoogle"
+                    >
+                      Sign in with Google    <img class="img-fluid justify-content-center" style="max-width: 20px;" src="../assets/login/google-icon.png">
+                    </button>
+                  </div>
+
+                </div>
               </div>
             </div>
           </div>
