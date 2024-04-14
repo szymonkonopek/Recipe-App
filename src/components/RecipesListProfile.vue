23ee90bb7b0446db584ef547f9625479cb38e3b5
szymonkonopek <szymon.konopek@memory2.co>
2024-04-14 12:43:20 +0200

    fix(userprofile): recipes render

--- src/components/RecipesListProfile.vue
+++ src/components/RecipesListProfile.vue
@@ -1,58 +1,56 @@
 <template>
   <div>
-
-    <div
-      v-if="recipes"
-    >
+    <div v-if="recipes && recipes.length == 0">
       <div class="alert alert-success text-black" role="alert">
         <h4 class="alert-heading">There is no receipes to display!</h4>
-        <p>Add your favorite recipes just by clicking <i class="bi bi-plus-circle display-10"></i> icon, down below.</p>
+        <p>
+          Add your favorite recipes just by clicking
+          <i class="bi bi-plus-circle display-10"></i> icon, down below.
+        </p>
       </div>
     </div>
 
     <div
       v-else
-      v-for="(recipe) in recipes"
+      v-for="recipe in recipes"
       :key="recipe.id"
       class=""
       style="width: 100%; max-width: 50rem"
     >
       <Recipe :recipe="recipe"></Recipe>
     </div>
-
   </div>
 </template>
 
 <script>
-import { actionTypes } from '@/store/modules/firebasedb'
-import { mapState } from 'vuex'
-import Recipe from '@/components/Recipe.vue'
+import { actionTypes } from '@/store/modules/firebasedb';
+import { mapState } from 'vuex';
+import Recipe from '@/components/Recipe.vue';
 
 export default {
   name: 'AppRecipesListProfileView',
   props: {
     uid: {
       type: String,
-      required: false
+      required: false,
     },
     onlyMyRecipes: {
       type: Boolean,
-      required: false
-    }
+      required: false,
+    },
   },
   components: {
-    Recipe
+    Recipe,
   },
   computed: {
     ...mapState({
-      recipes: (state) => state.firebasedb.recipes
-    })
+      recipes: (state) => state.firebasedb.recipes,
+    }),
   },
-  created () {
+  created() {
     this.$store.dispatch(actionTypes.getRecipesByUserId, {
-      uid: this.uid
-    })
-  }
-
-}
+      uid: this.uid,
+    });
+  },
+};
 </script>
