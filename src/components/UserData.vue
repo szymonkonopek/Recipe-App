5eecc3be2a6182a8029bc8d88f24056110511125
szymonkonopek <szymon.konopek@memory2.co>
2024-04-14 12:40:51 +0200

    fix(userprofile): fix margin

--- src/components/UserData.vue
+++ src/components/UserData.vue
@@ -1,73 +1,76 @@
 <template>
-  <div class="container justify-content-center mx-2" v-if="userDetails">
+  <div class="container justify-content-center" v-if="userDetails">
     <div class="row justify-content-center">
-
       <div class="col-md-4 text-center">
-        <img src="../assets/profile/profile-photo.png" class="img-fluid justify-content-center" style="max-width: 80px;">
+        <img
+          src="../assets/profile/profile-photo.png"
+          class="img-fluid justify-content-center"
+          style="max-width: 80px"
+        />
       </div>
 
       <div class="col-md-8 justify-content-center m-0">
-
-        <div class="badge bg-secondary text-wrap m-0">
-          Created
-        </div>
+        <div class="badge bg-secondary text-wrap m-0">Created</div>
 
         <div class="h6 m-0">{{ userDetails.created }}</div>
 
-        <div class="badge bg-secondary text-wrap mx-0">
-          Last login
-        </div>
+        <div class="badge bg-secondary text-wrap mx-0">Last login</div>
 
         <div class="h6 m-0">{{ userDetails.lastSignIn }}</div>
 
-        <div class="badge bg-secondary text-wrap mx-0">
-          Login provider
-        </div>
+        <div class="badge bg-secondary text-wrap mx-0">Login provider</div>
 
         <div class="h7 m-0" v-if="userDetails.providers">
-          <img v-if="userDetails.providers.includes('google.com')" src="../assets/profile/google-icon.png"
-               class="img-fluid justify-content-center" style="max-width: 40px;">
-          <img v-else src="../assets/profile/email-icon.png" class="img-fluid justify-content-center"
-               style="max-width: 40px;">
+          <img
+            v-if="userDetails.providers.includes('google.com')"
+            src="../assets/profile/google-icon.png"
+            class="img-fluid justify-content-center"
+            style="max-width: 40px"
+          />
+          <img
+            v-else
+            src="../assets/profile/email-icon.png"
+            class="img-fluid justify-content-center"
+            style="max-width: 40px"
+          />
         </div>
-
       </div>
-
-      
     </div>
   </div>
   <app-user-form></app-user-form>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-import { actionTypes } from '@/store/modules/firebasedb'
-import AppUserForm from '@/components/UserForm'
+import { mapActions } from 'vuex';
+import { actionTypes } from '@/store/modules/firebasedb';
+import AppUserForm from '@/components/UserForm';
 
 export default {
   name: 'AppUserData',
   components: { AppUserForm },
-  data () {
+  data() {
     return {
-      userDetails: null
-    }
+      userDetails: null,
+    };
   },
   methods: {
     ...mapActions({
-      getUserDetails: actionTypes.getUserDetails
+      getUserDetails: actionTypes.getUserDetails,
     }),
-    fetchUserDetails () {
-      this.getUserDetails().then(userDetails => {
-        this.userDetails = userDetails
-      }).catch(error => {
-        console.error(error)
-      })
-    }
+    fetchUserDetails() {
+      this.getUserDetails()
+        .then((userDetails) => {
+          this.userDetails = userDetails;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
   },
-  created () {
-    this.fetchUserDetails()
-  }
-}
+  created() {
+    this.fetchUserDetails();
+  },
+};
 </script>
 
 <style>
