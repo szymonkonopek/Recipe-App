4b48f03823374f7f179fe6113a7f71b10c0fd4a6
szymonkonopek <szymon.konopek@memory2.co>
2024-04-20 11:55:02 +0200

    fix(HomePage): fix wiktor's custom css classes and add bootstrap

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,78 +1,81 @@
 <!-- eslint-disable max-len -->
 <template>
-  <div class="container">
+  <div class="container d-flex flex-column align-items-center">
     <div id="cookingCarousel" class="mb-5">
       <img src="..\assets\carousel\BanerImg.png" class="d-block w-100" alt="Welcome to our Kitchen">
     </div>
 
-    <div class="bg-white rounded p-3 mb-3 shadow" style="background-color: #FEEBD3;">
-      <div id="containerCarousel1" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="..\assets\carousel\mock.jpg" class="d-block w-100" alt="Image 1">
+    <div class="row">
+    <div class="col-lg-4 mb-4">
+      <div class="bg-white rounded p-3 shadow">
+        <div id="containerCarousel1" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="..\assets\carousel\image5.jpg" class="d-block w-100" alt="Image 1">
+            </div>
+            <div class="carousel-item">
+              <img src="..\assets\carousel\image6.jpg" class="d-block w-100" alt="Image 2">
+            </div>
           </div>
-          <div class="carousel-item">
-            <img src="..\assets\carousel\mbape.jpg" class="d-block w-100" alt="Image 2">
-          </div>
-
+          <button class="carousel-control-prev" type="button" data-bs-target="#containerCarousel1" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#containerCarousel1" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#containerCarousel1" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#containerCarousel1" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
       </div>
-      <p>essa</p>
     </div>
 
-    <div class="bg-white rounded p-3 mb-3 shadow" style="background-color: #E7F1F5;">
-      <div id="containerCarousel2" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="..\assets\carousel\klopsiki.jpg" class="d-block w-100" alt="Image 2">
+    <div class="col-lg-4 mb-4">
+      <div class="bg-white rounded p-3 shadow">
+        <div id="containerCarousel2" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="..\assets\carousel\image1.jpg" class="d-block w-100" alt="Image 2">
+            </div>
+            <div class="carousel-item">
+              <img src="..\assets\carousel\image2.jpg" class="d-block w-100" alt="Image 2">
+            </div>
           </div>
-          <div class="carousel-item">
-            <img src="..\assets\carousel\mbape.jpg" class="d-block w-100" alt="Image 2">
-          </div>
-
+          <button class="carousel-control-prev" type="button" data-bs-target="#containerCarousel2" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#containerCarousel2" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#containerCarousel2" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#containerCarousel2" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
       </div>
-      <p>Rizz</p>
     </div>
 
-    <div class="bg-white rounded p-3 mb-3 shadow" style="background-color: #FADBD8;">
-      <div id="containerCarousel3" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="..\assets\carousel\burger.jpg" class="d-block w-100" alt="Image 3">
+    <div class="col-lg-4  mb-4">
+      <div class="bg-white rounded p-3 shadow">
+        <div id="containerCarousel3" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="..\assets\carousel\image3.jpg" class="d-block w-100" alt="Image 3">
+            </div>
+            <div class="carousel-item">
+              <img src="..\assets\carousel\image4.jpg" class="d-block w-100" alt="Image 2">
+            </div>
           </div>
-          <div class="carousel-item">
-            <img src="..\assets\carousel\mbape.jpg" class="d-block w-100" alt="Image 2">
-          </div>
-
+          <button class="carousel-control-prev" type="button" data-bs-target="#containerCarousel3" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#containerCarousel3" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#containerCarousel3" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#containerCarousel3" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
       </div>
-      <p>Sigma</p>
     </div>
+  </div>
+
   </div>
 </template>
 
