f3d9e36ce89ba2beba35e790fb9505b18713a2ef
wiktor6655 <wiktorkrupa1@gmail.com>
2024-04-25 22:30:25 +0200

    ikony

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,13 @@ module.exports = defineConfig({
   pwa: {
     name: 'Recipe App',
     themeColor: '#e38b29',
+    iconPaths: {
+      favicon32: 'mitten.ico', // Path to your 32x32 favicon
+      favicon16: 'mitten.ico', // Path to your 16x16 favicon
+      appleTouchIcon: 'mitten.png', // Path to your Apple touch icon (152x152)
+      maskIcon: 'cookingIcon1.svg', // Path to your maskable icon (SVG format)
+      msTileImage: 'cookingIcon144.png', // Path to your Microsoft Tile icon (144x144)
+    }
   },
   // devServer: {
   //   https: true,
